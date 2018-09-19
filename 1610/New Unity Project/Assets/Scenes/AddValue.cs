56fa746c934fd35fdbe8699bd69194cc44408e2a
Tyler Hales <10869186@my.uvu.edu>
2018-09-19 15:40:34 -0600

    Mouse Events and Such

---
+++ 1610/New Unity Project/Assets/Scenes/AddValue.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class AddValue : ScriptableObject {
+
+    public FloatData ValueObj;
+
+    public FloatData MinValue;
+    public FloatData MaxValue;
+
+    public UnityEvent EventMin;
+    public UnityEvent EventMax;
+
+    public void AddValueToObj (FloatData data)
+    {
+        ValueObj.Value += data.Value;
+
+        if(ValueObj.Value <= MinValue.Value)
+        {
+            EventMin.Invoke();
+        }
+        if(ValueObj.Value >= MaxValue.Value)
+        {
+            EventMax.Invoke();
+        }
+    }
+
+}
