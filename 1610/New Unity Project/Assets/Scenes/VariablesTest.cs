e01ba93803f2bb4820a6691d4c1a03b055c64dae
Tyler Hales <10869186@my.uvu.edu>
2018-09-25 21:41:22 -0600

    9/25
    Variables and such testing

---
+++ 1610/New Unity Project/Assets/Scenes/VariablesTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesTest : MonoBehaviour {
+    public bool IsTheChosenOne;
+
+    public int PowerCount = 1;
+
+    public float Energy = 1.0f;
+    public float Power;
+
+    public string PlayerName = "Trackneon";
+
+    public Vector3 Location;
+
+    private void Start()
+    {
+        print(PlayerName);
+        print("has");
+        print(Power + " power");
+
+        if (IsTheChosenOne)
+        {
+            print(PlayerName + " is the Chosen One.");
+        }
+    }
+}
