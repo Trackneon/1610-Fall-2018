87e7b96e4405781960d6f976599c38bd8da86635
Tyler Hales <10869186@my.uvu.edu>
2018-10-11 21:10:08 -0600

    10-11

---
+++ 1610/New Unity Project/Assets/Scenes/WhatCanSeeIt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatCanSeeIt : ScriptableObject {
+
+    private float health;
+    protected float PowerLevel;
+    public float EnergyLevel;
+
+    private void AddHealth()
+    {
+        health += 100;
+    }
+
+    protected void AddPower()
+    {
+        AddHealth();
+    }
+
+    public void AddEnergy()
+    {
+
+    }
+
+}
+
+
+public class ICanSeeIt : MonoBehaviour
+{
+    private WhatCanSeeIt newobject;
+
+    private void Start()
+    {
+        newobject.EnergyLevel = 100;
+        newobject.AddEnergy();
+    }
+}
+
+public class Child : WhatCanSeeIt
+{
+    void CallThings()
+    {
+        PowerLevel = 200;
+        EnergyLevel = 300;
+        AddPower();
+        AddEnergy();
+        
+    }
+}
