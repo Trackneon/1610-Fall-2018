7f60cfe9abea71c5902a3024ad416145bd32d0dc
Tyler Hales <10869186@my.uvu.edu>
2018-10-24 19:33:33 -0600

    10/24 In Class

---
+++ 1610/New Unity Project/Assets/Scenes/Loops.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+    // Use this for initialization
+
+    public string[] Names;
+    public GameObject[] Things;
+
+	void Start () {
+        for (int i = 0; i < Names.Length; i++)
+        {
+            print(Names[i]);
+        }
+
+        foreach (var thing in Things)
+        {
+            print(thing);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
